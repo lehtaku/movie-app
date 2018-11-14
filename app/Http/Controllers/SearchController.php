6db92632232fc7f32961c0f47de559bc52264015
lehtaku <akulehtonen@hotmail.com>
2018-11-14 20:52:11 +0200

    Update search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -20,21 +20,31 @@ class SearchController extends Controller
 
     public function searchByKeyword(Request $request) {
 
-        $encodedKeyword = urlencode($request->query('keyword'));
+        // Get search parameters from request
+        $keyword = urlencode($request->query('keyword'));
+        $movieType = urlencode($request->query('type'));
 
-        $response = $this->client->request('GET', '?apikey=' . $this->apiKey . '&s=' . $encodedKeyword)->getBody();
+        // Make search request
+        $response = $this->client->request('GET', '?apikey=' . $this->apiKey . '&s=' . $keyword . '&type=' . $movieType)->getBody();
+        $json = json_decode($response, true);
 
-        return json_decode($response, true)['Search'];
+        // Check if empty
+        if ($json['Response'] === 'False') return $json['Error'];
 
+        // Return results if found any
+        return $json['Search'];
     }
 
     public function findById(Request $request) {
+        // Similar to search function but for finding specific movie
 
         $imdbId = $request->query('id');
 
-        $response = $this->client->request('GET', '?apikey=' . $this->apiKey . '&i=' . $imdbId)->getBody();
+        $response = $this->client->request('GET', '?apikey=' . $this->apiKey . '&i=' . $imdbId . '&plot=full')->getBody();
+        $json = json_decode($response, true);
 
-        return json_decode($response, true);
+        if ($json['Response'] === 'False') return $json['Error'];
 
+        return $json;
     }
 }
