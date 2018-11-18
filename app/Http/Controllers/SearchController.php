33df840cd07ce496bc63b5f9c20e176cd4bdc790
Leevi Kopakkala <leevi.kopakkala@hotmail.com>
2018-11-19 01:43:11 +0200

    Update Search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -1,54 +1,55 @@
 <?php
-
 namespace App\Http\Controllers;
-
+use App\Playlist;
 use Illuminate\Http\Request;
-
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Client;
-
 class SearchController extends Controller
 {
     private $client;
     private $apiKey;
-
     public function __construct()
     {
         $this->client = new Client(['base_uri' => 'http://www.omdbapi.com/']);
         $this->apiKey = config('app.omdb_key');
     }
-
-    public function searchByKeyword(Request $request) {
-
-        // Get search parameters from request
+    public function searchByKeyword(Request $request)
+    {
         $keyword = $request->keyword;
         if ($request->type !== null){
             $movieType = $request->type;
         } else {
             $movieType = '';
         }
-        
-        // Make search request
         $response = $this->client->request('GET', '?apikey=' . $this->apiKey . '&s=' . $keyword . '&type=' . $movieType)->getBody();
         $json = json_decode($response, true);
-
-        // Check if empty
         if ($json['Response'] === 'False') return $json['Error'];
-
-        // Return results if found any
         return $json['Search'];
     }
-
-    public function findById(Request $request) {
-
-        // Similar to search function but for finding specific movie
-        $imdbId = $request->movieId;
-
-        $response = $this->client->request('GET', '?apikey=' . $this->apiKey . '&i=' . $imdbId . '&plot=full')->getBody();
+    public function findById(Request $request)
+    {
+        $movieId = $request->query('movieId');
+        $userId = $this->getUserId();
+        $response = $this->client->request('GET', '?apikey=' . $this->apiKey . '&i=' . $movieId . '&plot=full')->getBody();
         $json = json_decode($response, true);
-
         if ($json['Response'] === 'False') return $json['Error'];
-
+        $json['InPlaylist'] = $this->checkIfWatched($userId, $movieId);
         return $json;
+    }
+    public function getUserId()
+    {
+        if(auth()->id() !== null) {
+            return auth()->id();
+        } else {
+            return null;
+        }
+    }
+    public function checkIfWatched($userId, $movieId)
+    {
+        $item = Playlist::where([
+            'movie_id' => $movieId,
+            'user_id' => $userId
+            ])->first();
+        return ($item == null ? false : true);
     }
 }
