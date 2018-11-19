4838b3e09fe67d723a62b74f6738a6c993bad841
Leevi Kopakkala <leevi.kopakkala@hotmail.com>
2018-11-19 03:03:48 +0200

    Test for production

--- app/Http/Middleware/JWTAuthenticate.php
+++ app/Http/Middleware/JWTAuthenticate.php
@@ -50,6 +50,8 @@ class JWTAuthenticate extends BaseMiddleware
 
         $this->events->fire('tymon.jwt.valid', $user);
 
-        return $next($request);
+        return $next($request)->header('Access-Control-Allow-Origin', '*')
+        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorizations');;
     }
 }
