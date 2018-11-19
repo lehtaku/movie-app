73dc74df0e419e1ed3612102ecbcd0dde3cf9e74
lehtaku <akulehtonen@hotmail.com>
2018-11-19 19:46:44 +0200

    Mail test

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -2,20 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Mail\DemoMail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
 class EmailController extends Controller
 {
-    public function sendEmail(){
-        $data = array('name'=>"Aku Lehtonen");
+    public function sendEmail() {
+        $message = "Hello from endumx";
 
-        Mail::send(['text'=>'mail'], $data, function($message) {
-            $message->to('akulehtonen@hotmail.com', 'Aku Lehtonen')->subject
-            ('Laravel Basic Testing Mail');
-            $message->from('admin@endumx.com','Endumx');
-        });
-        echo "Basic Email Sent. Check your inbox.";
+        mail('akulehtonen@hotmail.com', 'New laravel message', $message);
     }
 }
