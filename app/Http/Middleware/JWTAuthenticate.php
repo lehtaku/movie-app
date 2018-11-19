8ef67aff9d04cf8847813b13514af8f55f836ca8
Leevi Kopakkala <leevi.kopakkala@hotmail.com>
2018-11-19 03:18:31 +0200

    Update CORS

--- app/Http/Middleware/JWTAuthenticate.php
+++ app/Http/Middleware/JWTAuthenticate.php
@@ -49,9 +49,8 @@ class JWTAuthenticate extends BaseMiddleware
         }
 
         $this->events->fire('tymon.jwt.valid', $user);
-
-        return $next($request)->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorizations');;
+        $response = $next($request);
+        $response->header('Access-Control-Allow-Origin', '*');
+        return $response;
     }
 }
